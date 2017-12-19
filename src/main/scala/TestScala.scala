b8f791ffe4c9fb1f079c2154bd72d5671387e4e8
raghavnyati <raghavnyati90@gmail.com>
2017-12-18 21:39:20 -0800

    Random forest model added + scaling of feature vector done.

--- src/main/scala/TestScala.scala
+++ src/main/scala/TestScala.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.ml.classification.RandomForestClassifier
 import org.apache.spark.ml.feature.{VectorAssembler}
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.ml.feature.StandardScaler
 
 object TestScala {
 
@@ -35,9 +36,9 @@ object TestScala {
   }
 
   def createRandomForestModel() : RandomForestClassifier = {
-    val rf = new RandomForestClassifier().setLabelCol("label").
-      setFeaturesCol("features").
-      setImpurity("gini").setMaxDepth(3).setNumTrees(20).setFeatureSubsetStrategy("auto")
+    val rf = new RandomForestClassifier().setLabelCol("stars").
+      setFeaturesCol("scaledFeatures").
+      setImpurity("gini").setMaxDepth(20).setNumTrees(500).setFeatureSubsetStrategy("auto")
     rf
   }
 
@@ -65,5 +66,40 @@ object TestScala {
 
     //printDataframe(data)
     data.printSchema()
+    val df_assembled = creatingFeatureVector(data).select("features", "stars")
+    df_assembled.show(10)
+
+    val scaler = new StandardScaler().setInputCol("features").
+      setOutputCol("scaledFeatures").
+      setWithStd(true).
+      setWithMean(true)
+
+    val transformedDF =  scaler.fit(df_assembled).transform(df_assembled)
+
+    val Array(trainingData, testData) = transformedDF.randomSplit(Array(0.7, 0.3))
+    log.info("Training and testing data created successfully.")
+
+    // Train model. This also runs the indexers.
+    val model = createRandomForestModel().fit(trainingData)
+    log.info("Model trained success.")
+
+    println(model.featureImportances)
+
+    // Make predictions.
+    val predictions = model.transform(testData)
+    log.info("Applying model on test data success.")
+
+    val temp_df1 = predictions.select("stars", "prediction")
+    val temp_rdd1 = temp_df1.as[(Double,Double)].rdd
+    val temp_metrics = new MulticlassMetrics(temp_rdd1)
+    val labels = temp_metrics.labels
+
+    val confusionMatrix = sc.parallelize(temp_metrics.confusionMatrix.toArray)
+
+    println(confusionMatrix)
+
+    log.info("End")
+    spark.stop()
+    sc.stop()
   }
 }
