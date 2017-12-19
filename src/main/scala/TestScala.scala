9f7411a32d4fadd9b7c57ee2a846c64e4200b8b5
raghavnyati <raghavnyati90@gmail.com>
2017-12-18 18:45:23 -0800

    Reading csv file data

--- src/main/scala/TestScala.scala
+++ src/main/scala/TestScala.scala
@@ -1,12 +1,69 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.log4j.{Level, LogManager, Logger}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.feature.{VectorAssembler}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 
 object TestScala {
 
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf()
-    conf.setAppName("Yelp Test")
+  def createSparkSession() : SparkSession = {
+    return SparkSession.builder.appName("Yelp Help").getOrCreate()
+  }
+
+  def createSparkContext() : SparkContext = {
+    val conf = new SparkConf().setAppName("YelpHelp Context")
     conf.setMaster("local[2]")
     val sc = new SparkContext(conf)
-    println(sc)
+    sc
+  }
+
+  def readData(spark: SparkSession) : DataFrame = {
+    val reader = spark.read
+    reader.option("delimeter", "\t").
+      option("header", "true").
+      option("inferSchema","true")
+    return reader.csv("/Users/raghavnyati/Desktop/dataset/restaurants_data.csv")//.
+      //toDF("business_id", "review_count", "stars", "checkin_count", "city", "state", "categories")
+  }
+
+  def creatingFeatureVector(df_indexed: DataFrame) : DataFrame = {
+    val assembler = new VectorAssembler().
+      setInputCols(Array("review_count", "checkin_count","city", "state")).
+      setOutputCol("features")
+    assembler.transform(df_indexed)
+  }
+
+  def createRandomForestModel() : RandomForestClassifier = {
+    val rf = new RandomForestClassifier().setLabelCol("label").
+      setFeaturesCol("features").
+      setImpurity("gini").setMaxDepth(3).setNumTrees(20).setFeatureSubsetStrategy("auto")
+    rf
+  }
+
+  def printDataframe(df: DataFrame) = {
+    df.collect().foreach(println)
+  }
+
+  def main(args: Array[String]): Unit = {
+//    val conf = new SparkConf()
+//    conf.setAppName("Yelp Test")
+//    conf.setMaster("local[2]")
+//    val sc = new SparkContext(conf)
+//    println(sc)
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val log = LogManager.getRootLogger
+    log.info("Start")
+
+    val sc = createSparkContext()
+    val spark = createSparkSession()
+    log.info("Spark Context and session created successfully")
+    import spark.implicits._
+    val data = readData(spark)
+    log.info("File read success.")
+
+    //printDataframe(data)
+    data.printSchema()
   }
 }
